771eefece22385812277292515a862b82c65de1f
Ian <ian.daly@me.com>
2025-07-01 18:16:04 -0400

    Changed to work with new files

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from files import scan_files, Pdf
+from files import scan_files,get_files_to_include, extract_pdf_text, Pdf
+from parser import parse_results
 from typing import List
 
 def main():
     files: List[Pdf] = scan_files()
-    for file in files:
-        print(file.name)
+    chosen_files: list[Pdf] = get_files_to_include(files)
+    extract_pdf_text(chosen_files)
+    parse_results(chosen_files)
+
 
 if __name__ == "__main__":
     main()
