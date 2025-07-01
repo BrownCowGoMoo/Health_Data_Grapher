d9c470df75ea1364f6d1d83e9ca9c431fa593dd5
Ian <ian.daly@me.com>
2025-07-01 15:02:39 -0400

    Seperated the user input for the file path from the scan_files function and added get_files_to_include function

--- files.py
+++ files.py
@@ -9,17 +9,7 @@ class Pdf:
     name_w_ext: str
     name: str
 
-def get_files_from_dir() -> list[tuple[str, str, str]]:
-    """
-    Gets all pdf files from the given directory and returning the name with and without the extension.
-
-    Returns:
-        files: A list of tuples where each tuple contains the name with and without the extension.
-            tuple[0] = path to file
-            tuple[1] = name with extension
-            tuple[2] = name without extension
-    """
-
+def get_directory_path() -> str:
     while True:
 
         directory = input("Input the directory path to your files: ")
@@ -34,15 +24,28 @@ def get_files_from_dir() -> list[tuple[str, str, str]]:
         else:
             break
 
+    return directory
+
+def scan_files() -> list[Pdf]:
+
     files = [Pdf(str(file), file.name, file.stem)
-             for file in directory.iterdir()
+             for file in get_directory_path().iterdir()
              if file.is_file() and file.suffix.lower() == ".pdf"]
     
     if not files:
         sys.exit("There are no .pdf files within the directory.")
 
     return files
+
+def get_files_to_include(files: list[Pdf]) -> list[str]:
+    chosen_files = []
+    for file in files:
+        while True:
+            ans = input(f"Would you like to incude {file.name_w_ext}? (y/n): ")
+            
+
+
     
 
 if __name__ == "__main__":
-    print(get_files_from_dir())
+    print(scan_files())
