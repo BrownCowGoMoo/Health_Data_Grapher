b3a9a77e8680c4fcb221b461776b9064f74158bd
Ian <ian.daly@me.com>
2025-07-01 19:28:56 -0400

    Changed to work with new layout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from files import scan_files,get_files_to_include, extract_pdf_text
-from parser import parse_results
+from parser import parse_reports
 
 if TYPE_CHECKING:
     from models import Pdf
@@ -11,7 +11,7 @@ def main():
     files: list[Pdf] = scan_files()
     chosen_files: list[Pdf] = get_files_to_include(files)
     extract_pdf_text(chosen_files)
-    print(parse_results(chosen_files))
+    parse_reports(chosen_files)
 
 
 if __name__ == "__main__":
