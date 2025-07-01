bd13adb1896c373b8ce94d15c1fbba0a4a990b4a
Ian <ian.daly@me.com>
2025-07-01 19:19:49 -0400

    Split main loop logic into seperate function.

--- parser.py
+++ parser.py
@@ -5,9 +5,21 @@ import re
 from models import Pdf, ResultInfo, ResultInfoSeries
 
 
-resultRE = re.compile(r"(?P<name>[\w\s]+?)(?P<flag>HI|LO)?\s(?P<value>\d+\.?\d*)\s(?P<lower_range>\d+\.?\d*)\s*-\s*(?P<upper_range>\d+\.?\d*)\s(?P<units>.+)")
+def parse_reports(chosen_files: list[Pdf]) -> list[ResultInfoSeries]:
+    all_series: list[ResultInfoSeries] = []
 
-def parse_results(chosen_files: list[Pdf]) -> list[ResultInfoSeries]:
+    for file in chosen_files:
+        series = ResultInfoSeries(file.name)
+        for line in file.text:
+            info = parse_results(line)
+            if not info:
+                continue
+            series.report_results.append(info)
+        all_series.append(series)
+    return all_series
+            
+
+def parse_results(line: str) -> ResultInfo:
     """
     Parses the wanted results from each chosen file, converts info to proper types.
 
@@ -17,22 +29,20 @@ def parse_results(chosen_files: list[Pdf]) -> list[ResultInfoSeries]:
     Returns:
         all_series: list[ResultInfoSeries] = List of all Result info Series objects.
     """
-    all_series: list[ResultInfoSeries] = []
-    for file in chosen_files:
-        series = ResultInfoSeries(file.name)
-        for line in file.text:
-            match = resultRE.search(line)
-            if not match:
-                continue
-            name, flag, value, lower_range, upper_range, units = match.groups()
-            try:
-                value = float(value)
-                lower_range = float(lower_range)
-                upper_range = float(upper_range)
-            except ValueError:
-                print("value error")
-            info = ResultInfo(name, flag, value, lower_range, upper_range, units)
-            series.report_results.append(info)
-        all_series.append(series)
-    return all_series
+    resultRE = re.compile(r"(?P<name>[\w\s]+?)(?P<flag>HI|LO)?\s(?P<value>\d+\.?\d*)\s(?P<lower_range>\d+\.?\d*)\s*-\s*(?P<upper_range>\d+\.?\d*)\s(?P<units>.+)")
+
+    match = resultRE.search(line)
+    if not match:
+        return None
+    name, flag, value, lower_range, upper_range, units = match.groups()
+    try:
+        value = float(value)
+        lower_range = float(lower_range)
+        upper_range = float(upper_range)
+    except ValueError:
+        print("value error")
+    info = ResultInfo(name, flag, value, lower_range, upper_range, units)
+
+    return info 
+ 
 
