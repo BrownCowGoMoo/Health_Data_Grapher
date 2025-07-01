9414f0fb5f75bbb016248bcfd4f9617417896ce4
Ian <ian.daly@me.com>
2025-07-01 18:31:06 -0400

    Added comments throughout.

--- parser.py
+++ parser.py
@@ -5,6 +5,17 @@ from files import Pdf
 
 @dataclass
 class ResultInfo:
+    """
+    Dataclass to hold parsed result values
+
+    Attrubutes:
+        name: str = Name of the result
+        flag: str = flag from the result 'HI' or 'LO'
+        value: float = result value
+        lower_range: float = lower healthy range for value
+        upper_range: float = upper healthy range for value
+        units: str = value units
+    """
     name: str
     flag: str
     value: float
@@ -14,12 +25,28 @@ class ResultInfo:
 
 @dataclass
 class ResultInfoSeries:
+    """
+    Dataclass to hold the report name and the list of all values parsed
+
+    Attributes:
+        report_name: str = name of the report
+        report_results: list[ResultInfo] = list of all result info objects associated with the report
+    """
     report_name: str
     report_results: list[ResultInfo] = field(default_factory=list)
 
 resultRE = re.compile(r"(?P<name>[\w\s]+?)(?P<flag>HI|LO)?\s(?P<value>\d+\.?\d*)\s(?P<lower_range>\d+\.?\d*)\s*-\s*(?P<upper_range>\d+\.?\d*)\s(?P<units>.+)")
 
-def parse_results(chosen_files: list[Pdf]):
+def parse_results(chosen_files: list[Pdf]) -> list[ResultInfoSeries]:
+    """
+    Parses the wanted results from each chosen file, converts info to proper types.
+
+    Args:
+        chosen_files: list[Pdf] = list of pdf objects of chosen files
+    
+    Returns:
+        all_series: list[ResultInfoSeries] = List of all Result info Series objects.
+    """
     all_series: list[ResultInfoSeries] = []
     for file in chosen_files:
         series = ResultInfoSeries(file.name)
