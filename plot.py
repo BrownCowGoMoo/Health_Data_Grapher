08b23cf99b75487006ba4e9d032524a599653c59
Ian <ian.daly@me.com>
2025-07-02 16:11:38 -0400

    added plot.py to contain graphing logic

---
+++ plot.py
@@ -0,0 +1,19 @@
+
+from __future__ import annotations
+from functools import reduce
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models import ResultInfoSeries
+
+def get_common_results(all_records: list[ResultInfoSeries]):
+    
+
+    common_names = [{info.name for info in records.report_results} for records in all_records]
+    print(common_names)
+
+    for item in all_records:
+        for info in item.report_results:
+            print(info.name)
+
+    
