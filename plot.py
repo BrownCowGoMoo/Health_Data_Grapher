704aa22213c27a4dd562aed7c20ad31754a6397a
Ian <ian.daly@me.com>
2025-07-02 16:59:36 -0400

    changed get_common_results to properly find all common names from every report.

--- plot.py
+++ plot.py
@@ -5,7 +5,10 @@ if TYPE_CHECKING:
     from models import ResultInfoSeries
 
 def get_common_results(all_records: list[ResultInfoSeries]):
-    common_names = [{info.name for info in records.report_results} for records in all_records]
+    name_sets = [{info.name for info in records.report_results} for records in all_records]
+    common_names = set.intersection(*name_sets)
+    print(common_names)
+
 
     
 
