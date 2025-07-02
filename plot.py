c797c40ded266dceb7c7786734489c399694248d
Ian <ian.daly@me.com>
2025-07-02 16:46:05 -0400

    removed test code

--- plot.py
+++ plot.py
@@ -1,16 +1,11 @@
-
 from __future__ import annotations
-from functools import reduce
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from models import ResultInfoSeries
 
 def get_common_results(all_records: list[ResultInfoSeries]):
-    
-
     common_names = [{info.name for info in records.report_results} for records in all_records]
-    print(common_names)
 
     
 
