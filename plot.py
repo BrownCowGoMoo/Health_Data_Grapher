34f048bda7874f5fed5be96afcba888cbb0256f9
Ian <ian.daly@me.com>
2025-07-02 19:43:44 -0400

    Removed unsed function.

--- plot.py
+++ plot.py
@@ -4,10 +4,9 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from models import ResultInfoSeries
 
-def get_common_results(all_records: list[ResultInfoSeries]):
-    name_sets = [{info.name for info in records.report_results} for records in all_records]
-    common_names = set.intersection(*name_sets)
-    print(common_names)
+
+
+
 
 
     
