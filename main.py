2d391a6623c8a0cc123cab4b5cc0657af026a74a
Ian <ian.daly@me.com>
2025-07-02 22:31:13 -0400

    Updated main to work with new fuctions and classes in plot.py.  Also added error checking and exit for no shared items.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+import sys
 from db import DBManager
 from files import scan_files,get_files_to_include, extract_pdf_text
 from parser import parse_reports
-from plot import get_items_to_prompt, sort_data_by_date
+from plot import  sort_data_by_date, PlotItemsSelector
 
 if TYPE_CHECKING:
     from models import Pdf, ResultInfoSeries
@@ -14,12 +15,18 @@ def main():
     chosen_files: list[Pdf] = get_files_to_include(files)
     extract_pdf_text(chosen_files)
     all_chosen_records: list[ResultInfoSeries] = parse_reports(chosen_files)
+
     db = DBManager()
     db.create_tables()
     db.insert_info(all_chosen_records)
     shared_names = db.select_shared_names()
-    get_items_to_prompt(shared_names)
-    sort_data_by_date(all_chosen_records)
+    if not shared_names:
+        print("There are no shared results across all of your reports.")
+        sys.exit()
+
+    item_to_plot = PlotItemsSelector.get_item_to_plot(shared_names)
+    
+
 
 
 if __name__ == "__main__":
